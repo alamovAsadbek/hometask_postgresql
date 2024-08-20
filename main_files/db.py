7fbc04dd9c531940de8be65c5e7ef58dad70497e
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-08-20 18:48:03 +0500

    Create admin.py in role/admin

--- main_files/db.py
+++ main_files/db.py
@@ -1,24 +1,46 @@
+import contextlib
+
 import psycopg2
 
 from config import config
+from main_files.decorator_func import log_decorator
 
 
-def connect():
-    global connection
-    try:
-        connection = None
-        params = config()
-        connection = psycopg2.connect(**params)
-        cursor = connection.cursor()
-        cursor.execute('SELECT VERSION()')
-        cursor.fetchone()
-        cursor.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(f'Error: {error}')
-    finally:
-        if connection is not None:
-            connection.close()
+class DatabaseManager:
 
+    # connect database
+    @contextlib.contextmanager
+    def connect(self):
+        global connection
+        try:
+            connection = None
+            params = config()
+            connection = psycopg2.connect(**params)
+            cursor = connection.cursor()
+            yield cursor
+            cursor.close()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(f'Error: {error}')
+        finally:
+            if connection is not None:
+                connection.close()
 
-if __name__ == '__main__':
-    connect()
+    # show all tables
+    @log_decorator
+    def check_table(self, table_name: str):
+        try:
+            with self.connect() as cursor:
+                cursor.execute('''''')
+                print(f'Table {table_name} has been dropped')
+        except psycopg2.errors.DuplicateDatabase:
+            print(f'Table {table_name} already exists')
+            return True
+        except psycopg2.DatabaseError:
+            print("Error while connecting to database")
+            return False
+
+    # create table
+    @log_decorator
+    def create_table(self):
+        with self.connect() as cursor:
+            cursor.execute('CREATE TABLE ')
