630a6cec5a2ff5fce6d99c746e0be7883208c480
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-08-20 21:35:01 +0500

    Change code in admin.py

--- role/admin/admin.py
+++ role/admin/admin.py
@@ -1,2 +1,13 @@
+from main_files.decorator_func import log_decorator
+
+data_type='''
+1. 
+'''
 class Admin:
-    pass
+    @log_decorator
+    def check_data_type(self):
+        pass
+
+    @log_decorator
+    def create_table(self):
+        table_name: str = input("Table name: ").strip()
