02f93e75efa6073e54af43e0e913c6768a7850e7
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-08-20 16:14:24 +0500

    Change main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+# auth_menu
+def auth_menu():
+    pass
+
+
+# admin menu
+def admin_menu():
+    pass
--- main_files/db.py
+++ main_files/db.py
@@ -1,5 +1,24 @@
+import psycopg2
+
 from config import config
 
 
 def connect():
-    config()
+    global connection
+    try:
+        connection = None
+        params = config()
+        connection = psycopg2.connect(**params)
+        cursor = connection.cursor()
+        cursor.execute('SELECT VERSION()')
+        cursor.fetchone()
+        cursor.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(f'Error: {error}')
+    finally:
+        if connection is not None:
+            connection.close()
+
+
+if __name__ == '__main__':
+    connect()
