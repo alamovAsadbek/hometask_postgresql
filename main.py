97dd9937862236da8f439600c26690f9b43cb5ce
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-08-20 16:18:02 +0500

    Create file decorator file

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 # auth_menu
 def auth_menu():
-    pass
+    text = '''
+1. Register
+2. Login
+3. Logout
+    '''
+    print(text)
+    try:
+        pass
+    except ValueError:
+        print('Invalid menu')
+        auth_menu()
+    except Exception as e:
+        print(f'Error: {e}')
+        auth_menu()
 
 
 # admin menu
 def admin_menu():
+    pass
+
+
+# developer menu
+def developer_menu():
+    pass
+
+
+# user menu
+def user_menu():
     pass
