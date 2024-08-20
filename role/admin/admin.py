528f05e3bc8852949a23de860de96f33a7f073e7
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-08-20 21:49:43 +0500

    Create function create_table

--- role/admin/admin.py
+++ role/admin/admin.py
@@ -1,8 +1,10 @@
 from main_files.decorator_func import log_decorator
 
-data_type='''
+data_type = '''
 1. 
 '''
+
+
 class Admin:
     @log_decorator
     def check_data_type(self):
@@ -10,4 +12,25 @@ class Admin:
 
     @log_decorator
     def create_table(self):
+        table_data: dict = dict()
         table_name: str = input("Table name: ").strip()
+        print("Enter column👇: ")
+        print("Type exit to exit❌")
+        while True:
+            column_name: str = input("Column name: ").strip()
+            if column_name == "":
+                print("Please enter a valid column name.")
+                continue
+            elif column_name == "exit":
+                print("Exit")
+                break
+            column_type: str = input("Column type: ").strip()
+            if column_type == "":
+                print("Please enter a valid column type.")
+                continue
+            elif column_type == "exit":
+                print("Exit")
+                break
+            table_data[column_name] = column_type
+
+        print(table_data)
